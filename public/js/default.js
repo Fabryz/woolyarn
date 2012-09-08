ed82fe07114a10b710d3af1df32dee50da71aaac
Fabrizio Codello <fabryz@gmail.com>
2012-09-08 20:15:28 +0200

    refactor: debug object

--- public/js/default.js
+++ public/js/default.js
@@ -1,63 +1,56 @@
 $(document).ready(function() {
 
-	/*
-	* Functions
-	*/
+	var Debug = {
 
-	function log(msg) {
-		console.log(new Date().toJSON() +": "+ msg);
-	}
+		log: function (msg) {
+			console.log(new Date().toJSON() +": "+ msg);
+		},
 
-	function toggleDebug(spd) {
-		var speed = spd || 'fast';
-	
-		defaultDebug.fadeToggle(speed);
-		defaultDebug.toggleClass("active");
-		if (defaultDebug.hasClass("active")) {
+		toggle: function(speed) {
+			speed = speed || 'fast';
+		
+			defaultDebug.slideToggle(speed);
+		},
 
-		} else {
+		inject: function() {
+			status = $('<div/>', {
+				id: 'status'
+			});
 
+			clientId = $('<div/>', {
+				id: 'clientId'
+			});
+
+			tot = $('<span/>', {
+				id: 'tot',
+				text: '0'
+			});
+
+			online = $('<div/>', {
+				id: 'online',
+				text: ' players online.'
+			});
+
+			online.prepend(tot);
+
+			defaultDebug = $('<div/>', {
+				id: 'default-debug'
+			}).append( status )
+			  .append( clientId )
+			  .append( online );
+
+			$('body').append( defaultDebug );
 		}
-	}
-
-	function insertDebug() {
-		status = $('<div/>', {
-			id: 'status'
-		});
-
-		clientId = $('<div/>', {
-			id: 'clientId'
-		});
-
-		tot = $('<span/>', {
-			id: 'tot',
-			text: '0'
-		});
-
-		online = $('<div/>', {
-			id: 'online',
-			text: ' players online.'
-		});
-
-		online.prepend(tot);
-
-		defaultDebug = $('<div/>', {
-			id: 'default-debug'
-		}).append( status )
-		  .append( clientId )
-		  .append( online );
-
-		$('body').append( defaultDebug );
-	}
+	};
 
 	function init() {
-		insertDebug();
+		Debug.inject();
 
 		status.html("Connecting...");
 
 		$(document).keyup(function(e) {
 			if (e.keyCode === 220) { //backslash
-				toggleDebug();
+				Debug.toggle();
 			}
 		});
 	}
@@ -85,17 +78,17 @@ $(document).ready(function() {
 
 	socket.on('connect', function() {
 		status.html("Connected.");
-		log("Connected.");
+		Debug.log("Connected.");
 	});
 
 	socket.on('disconnect', function() {
 		status.html("Disconnected.");
-		log("Disconnected.");
+		Debug.log("Disconnected.");
 	});
 
 	socket.on('tot', function(data) {	
 		tot.html(data.tot);
-		log("Current players number: "+ data.tot);
+		Debug.log("Current players number: "+ data.tot);
 	});
 
 	socket.on('join', function(data) {
@@ -103,7 +96,7 @@ $(document).ready(function() {
 
 		clientId.html(data.player.id);
 
-		log('You have joined the server. (id: '+ data.player.id +').');
+		Debug.log('You have joined the server. (id: '+ data.player.id +').');
 	});
 
 	socket.on('quit', function(data) {
@@ -118,7 +111,7 @@ $(document).ready(function() {
 			}
 		}
 
-		log('< Player quitted: '+ quitter +' (id: '+ data.id +').');
+		Debug.log('< Player quitted: '+ quitter +' (id: '+ data.id +').');
 	});
 
 	socket.on('newplayer', function(data) {
@@ -126,7 +119,7 @@ $(document).ready(function() {
 		newPlayer = jQuery.extend(true, {}, data.player);
 		players.push(newPlayer);
 
-		log('> New player joined: '+ newPlayer.nick +' (id: '+ newPlayer.id +').');
+		Debug.log('> New player joined: '+ newPlayer.nick +' (id: '+ newPlayer.id +').');
 		
 		newPlayer = {};
 	});
@@ -143,7 +136,7 @@ $(document).ready(function() {
 			tmpPlayer = {};
 		}
 
-		log('Initial player list received: '+ length +' players.');
+		Debug.log('Initial player list received: '+ length +' players.');
 	});
 
 });
