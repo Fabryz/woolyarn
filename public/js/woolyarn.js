3875263798215db3bcb3d095688e51dc32f0db78
Fabrizio Codello <fabryz@gmail.com>
2013-07-14 18:33:43 +0200

    added date.json also on server debug.log

--- public/js/woolyarn.js
+++ public/js/woolyarn.js
@@ -7,240 +7,240 @@
 
 (function(exports) {
 
-	var Player = function(id, nick) {
-		this.id = id || 0;
-		this.nick = 'player'+ this.id || nick;
-		this.createdAt = Date.now();
-		this.updatedAt = Date.now();
-	};
+    var Player = function(id, nick) {
+        this.id = id || 0;
+        this.nick = 'player'+ this.id || nick;
+        this.createdAt = Date.now();
+        this.updatedAt = Date.now();
+    };
 
-	Player.prototype.toString = function() {
-		return '('+ this.id +') '+ this.nick;
-	};
+    Player.prototype.toString = function() {
+        return '('+ this.id +') '+ this.nick;
+    };
 
-	exports.Player = Player;
+    exports.Player = Player;
 })(typeof global === "undefined" ? window : exports);
 
 (function(exports) {
 
-	var Woolyarn = {
-		socket: null,
-		player: null,
-		players: [],
-		totalPlayers : 0,
+    var Woolyarn = {
+        socket: null,
+        player: null,
+        players: [],
+        totalPlayers : 0,
 
-		Player: function(id, nick) {
-			return new exports.Player(id, nick);
-		},
+        Player: function(id, nick) {
+            return new exports.Player(id, nick);
+        },
 
-		getSocket: function() {
-			return this.socket;
-		},
-		setSocket: function(socket) {
-			this.socket = socket;
-		},
+        getSocket: function() {
+            return this.socket;
+        },
+        setSocket: function(socket) {
+            this.socket = socket;
+        },
 
-		client: {
-			init: function(host) {
-				var debug = this.debug;
+        client: {
+            init: function(host) {
+                var debug = this.debug;
 
-				debug.init();
+                debug.init();
 
-				Woolyarn.client.connect(host || window.location.origin);
+                Woolyarn.client.connect(host || window.location.origin);
 
-				Woolyarn.player = new Player();
+                Woolyarn.player = new Player();
 
-				Woolyarn.socket.on('connect', function() {
-					debug.status.html("Connected.");
-					debug.log("Connected.");
-				});
+                Woolyarn.socket.on('connect', function() {
+                    debug.status.html("Connected.");
+                    debug.log("Connected.");
+                });
 
-				Woolyarn.socket.on('disconnect', function() {
-					debug.status.html("Disconnected.");
-					debug.log("Disconnected.");
-				});
+                Woolyarn.socket.on('disconnect', function() {
+                    debug.status.html("Disconnected.");
+                    debug.log("Disconnected.");
+                });
 
-				Woolyarn.socket.on('tot', function(total) {
-					// totalPlayers = total;
-					debug.tot.html(total);
-					debug.log("Current players number: "+ total);
-				});
+                Woolyarn.socket.on('tot', function(total) {
+                    // totalPlayers = total;
+                    debug.tot.html(total);
+                    debug.log("Current players number: "+ total);
+                });
 
-				Woolyarn.socket.on('join', function(player) {
-					Woolyarn.player = jQuery.extend(true, {}, player);
+                Woolyarn.socket.on('join', function(player) {
+                    Woolyarn.player = jQuery.extend(true, {}, player);
 
-					debug.playerId.html(player.id);
+                    debug.playerId.html(player.id);
 
-					debug.log('You have joined the server. (id: '+ player.id +').');
-				});
+                    debug.log('You have joined the server. (id: '+ player.id +').');
+                });
 
-				Woolyarn.socket.on('quit', function(data) {
-					var quitter = '';
+                Woolyarn.socket.on('quit', function(data) {
+                    var quitter = '';
 
-					var length = Woolyarn.players.length;
-					for(var i = 0; i < length; i++) {
-						if (Woolyarn.players[i].id == data.id) {
-							quitter = Woolyarn.players[i].nick;
-							Woolyarn.players.splice(i, 1);
-							break;
-						}
-					}
+                    var length = Woolyarn.players.length;
+                    for(var i = 0; i < length; i++) {
+                        if (Woolyarn.players[i].id == data.id) {
+                            quitter = Woolyarn.players[i].nick;
+                            Woolyarn.players.splice(i, 1);
+                            break;
+                        }
+                    }
 
-					debug.log('< Player quitted: '+ quitter +' (id: '+ data.id +').');
-				});
+                    debug.log('< Player quitted: '+ quitter +' (id: '+ data.id +').');
+                });
 
-				Woolyarn.socket.on('newplayer', function(player) {
-					var newPlayer = new Player();
-					newPlayer = jQuery.extend(true, {}, player);
-					Woolyarn.players.push(newPlayer);
+                Woolyarn.socket.on('newplayer', function(player) {
+                    var newPlayer = new Player();
+                    newPlayer = jQuery.extend(true, {}, player);
+                    Woolyarn.players.push(newPlayer);
 
-					debug.log('> New player joined: '+ newPlayer.nick +' (id: '+ newPlayer.id +').');
+                    debug.log('> New player joined: '+ newPlayer.nick +' (id: '+ newPlayer.id +').');
 
-					newPlayer = {};
-				});
+                    newPlayer = {};
+                });
 
-				Woolyarn.socket.on('playerlist', function(data) {
-					Woolyarn.players = []; //prepare for new updated list
+                Woolyarn.socket.on('playerlist', function(data) {
+                    Woolyarn.players = []; //prepare for new updated list
 
-					var length = data.list.length;
-					for(var i = 0; i < length; i++) {
-						var tmpPlayer = new Player();
-						tmpPlayer = jQuery.extend(true, {}, data.list[i]);
-						Woolyarn.players.push(tmpPlayer);
+                    var length = data.list.length;
+                    for(var i = 0; i < length; i++) {
+                        var tmpPlayer = new Player();
+                        tmpPlayer = jQuery.extend(true, {}, data.list[i]);
+                        Woolyarn.players.push(tmpPlayer);
 
-						tmpPlayer = {};
-					}
+                        tmpPlayer = {};
+                    }
 
-					debug.log('Initial player list received: '+ length +' players.');
-				});
-			},
+                    debug.log('Initial player list received: '+ length +' players.');
+                });
+            },
 
-			connect: function(host) {
-				Woolyarn.socket = new io.connect(host);
-				return Woolyarn.socket;
-			},
+            connect: function(host) {
+                Woolyarn.socket = new io.connect(host);
+                return Woolyarn.socket;
+            },
 
-			debug: {
-				status: null,
-				playerId: null,
-				tot: null,
-				online: null,
-				guiHandle: null,
+            debug: {
+                status: null,
+                playerId: null,
+                tot: null,
+                online: null,
+                guiHandle: null,
 
-				init: function() {
-					this.inject();
-					this.status.html("Connecting...");
+                init: function() {
+                    this.inject();
+                    this.status.html("Connecting...");
 
-					var that = this;
-					$(document).keyup(function(e) {
-						if (e.keyCode === 220) { //backslash
-							that.guiHandle.slideToggle('fast');
-						}
-					});
-				},
-				log: function (msg) {
-					if (window.console && window.console.log) {
-                    	window.console.log(new Date().toJSON() +": "+ msg);
-                	}
-				},
-				inject: function() {
-					this.status = $('<div/>', { id: 'status'	});
-					this.playerId = $('<div/>', { id: 'playerId' });
-					this.tot = $('<span/>', { id: 'tot', text: '0' });
-					this.online = $('<div/>', { id: 'online', text: ' players online.' });
+                    var that = this;
+                    $(document).keyup(function(e) {
+                        if (e.keyCode === 220) { //backslash
+                            that.guiHandle.slideToggle('fast');
+                        }
+                    });
+                },
+                log: function (msg) {
+                    if (window.console && window.console.log) {
+                        window.console.log(new Date().toJSON() +": "+ msg);
+                    }
+                },
+                inject: function() {
+                    this.status = $('<div/>', { id: 'status'    });
+                    this.playerId = $('<div/>', { id: 'playerId' });
+                    this.tot = $('<span/>', { id: 'tot', text: '0' });
+                    this.online = $('<div/>', { id: 'online', text: ' players online.' });
 
-					this.online.prepend(this.tot);
+                    this.online.prepend(this.tot);
 
-					this.guiHandle = $('<div/>', {
-						id: 'woolyarn-debug'
-					}).append( this.status )
-					.append( this.playerId )
-					.append( this.online );
+                    this.guiHandle = $('<div/>', {
+                        id: 'woolyarn-debug'
+                    }).append( this.status )
+                    .append( this.playerId )
+                    .append( this.online );
 
-					$('body').append( this.guiHandle );
-				}
-			}
-		},
-		server: {
-			client: null,
-			on: function(event, callback) {
-				Woolyarn.server.client.on(event, callback);
-			},
-			init: function(io, callback) {
+                    $('body').append( this.guiHandle );
+                }
+            }
+        },
+        server: {
+            client: null,
+            on: function(event, callback) {
+                Woolyarn.server.client.on(event, callback);
+            },
+            init: function(io, callback) {
 
-				io.sockets.on('connection', function(client) {
-					Woolyarn.server.client = client;
+                io.sockets.on('connection', function(client) {
+                    Woolyarn.server.client = client;
 
-					Woolyarn.player = new Woolyarn.Player(client.id);
+                    Woolyarn.player = new Woolyarn.Player(client.id);
 
-					Woolyarn.server.newPlayer(client, Woolyarn.player);
-					Woolyarn.server.sendPlayerList(client);
+                    Woolyarn.server.newPlayer(client, Woolyarn.player);
+                    Woolyarn.server.sendPlayerList(client);
 
-					Woolyarn.totalPlayers++;
-					Woolyarn.server.debug.log('+ '+ client.id +' connected, total players: '+ Woolyarn.totalPlayers);
+                    Woolyarn.totalPlayers++;
+                    Woolyarn.server.debug.log('+ '+ client.id +' connected, total players: '+ Woolyarn.totalPlayers);
 
-					io.sockets.emit('tot', Woolyarn.totalPlayers);
+                    io.sockets.emit('tot', Woolyarn.totalPlayers);
 
-					client.on('disconnect', function() {
-						var quitter = '';
+                    client.on('disconnect', function() {
+                        var quitter = '';
 
-						var length = Woolyarn.players.length;
-						for(var i = 0; i < length; i++) {
-							if (Woolyarn.players[i].id == client.id) {
-								quitter = Woolyarn.players[i].nick;
-								Woolyarn.players.splice(i, 1);
-								break;
-							}
-						}
+                        var length = Woolyarn.players.length;
+                        for(var i = 0; i < length; i++) {
+                            if (Woolyarn.players[i].id == client.id) {
+                                quitter = Woolyarn.players[i].nick;
+                                Woolyarn.players.splice(i, 1);
+                                break;
+                            }
+                        }
 
-						Woolyarn.totalPlayers--;
-						client.broadcast.emit('quit', { id: client.id });
-						io.sockets.emit('tot', Woolyarn.totalPlayers);
-						Woolyarn.server.debug.log('< '+ quitter +' ('+ client.id +') disconnected, total players: '+ Woolyarn.totalPlayers);
-					});
+                        Woolyarn.totalPlayers--;
+                        client.broadcast.emit('quit', { id: client.id });
+                        io.sockets.emit('tot', Woolyarn.totalPlayers);
+                        Woolyarn.server.debug.log('< '+ quitter +' ('+ client.id +') disconnected, total players: '+ Woolyarn.totalPlayers);
+                    });
 
-					callback(client);
-				});
+                    callback(client);
+                });
 
-			},
-			getPlayerById: function(id) {
-				var length = Woolyarn.players.length;
-				for(var i = 0; i < length; i++) {
-					if (Woolyarn.players[i].id == id) {
-						return Woolyarn.players[i];
-					}
-				}
+            },
+            getPlayerById: function(id) {
+                var length = Woolyarn.players.length;
+                for(var i = 0; i < length; i++) {
+                    if (Woolyarn.players[i].id == id) {
+                        return Woolyarn.players[i];
+                    }
+                }
 
-				return null;
-			},
-			sendPlayerList: function(client) {
-				client.emit('playerlist', { list: Woolyarn.players });
-				this.debug.log('* Sent player list to '+ client.id);
-			},
-			newPlayer: function(client, player) {
-				Woolyarn.players.push(player);
+                return null;
+            },
+            sendPlayerList: function(client) {
+                client.emit('playerlist', { list: Woolyarn.players });
+                this.debug.log('* Sent player list to '+ client.id);
+            },
+            newPlayer: function(client, player) {
+                Woolyarn.players.push(player);
 
-				client.emit('join', player );
-				client.broadcast.emit('newplayer', player);
+                client.emit('join', player );
+                client.broadcast.emit('newplayer', player);
 
-				this.debug.log('> New player: '+ player.nick);
-			},
-			getClient: function() {
-				return Woolyarn.server.client;
-			},
+                this.debug.log('> New player: '+ player.nick);
+            },
+            getClient: function() {
+                return Woolyarn.server.client;
+            },
 
-			debug: {
-				init: function() {
+            debug: {
+                init: function() {
 
-				},
-				log: function(msg) {
-					console.log(msg);
-				}
-			}
+                },
+                log: function(msg) {
+                    console.log(new Date().toJSON() +": "+ msg);
+                }
+            }
 
-		}
+        }
 
-	};
+    };
 
-	exports.Woolyarn = Woolyarn;
+    exports.Woolyarn = Woolyarn;
 })(typeof global === "undefined" ? window : exports);
