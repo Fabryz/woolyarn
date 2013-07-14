cf63ee69abe8aba28dd2cd15b59b7ee1028714b9
Fabrizio Codello <fabryz@gmail.com>
2013-07-14 18:18:57 +0200

    converted intentation to spaces

--- server.js
+++ server.js
@@ -3,31 +3,31 @@
 */
 
 var express = require('express'),
-	socketio = require('socket.io'),
-	http = require('http'),
+    socketio = require('socket.io'),
+    http = require('http'),
     path = require('path');
 
 var app = express(),
-	Woolyarn = require('./public/js/woolyarn.js').Woolyarn;
+    Woolyarn = require('./public/js/woolyarn.js').Woolyarn;
 
 // Express configuration
 
 app.configure(function(){
-	app.set('port', process.env.PORT || 8080);
-	app.use(express.favicon());
-	app.use(express.logger('short'));
-	app.use(express.bodyParser());
-	app.use(express.methodOverride());
-	app.use(app.router);
-	app.use(express.static(path.join(__dirname, 'public')));
+    app.set('port', process.env.PORT || 8080);
+    app.use(express.favicon());
+    app.use(express.logger('short'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
-	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.configure('production', function(){
-	app.use(express.errorHandler());
+    app.use(express.errorHandler());
 });
 
 // Routes
@@ -44,20 +44,20 @@ var server = http.createServer(app).listen(app.get('port'), function(){
 * Socket.IO
 */
 
-var	io = socketio.listen(server);
+var io = socketio.listen(server);
 
 io.configure(function() {
-	io.enable('browser client minification');
-	io.set('log level', 1);
+    io.enable('browser client minification');
+    io.set('log level', 1);
 });
 
 Woolyarn.server.init(io, function(client) {
 
-	// Here you can listen for your events
+    // Here you can listen for your events
 
-	Woolyarn.server.on('customEvent', function(data) {
-		console.log('Incoming customEvent from: '+ client.id);
+    Woolyarn.server.on('customEvent', function(data) {
+        console.log('Incoming customEvent from: '+ client.id);
 
-		io.sockets.emit('customEvent', client.id);
-	});
+        io.sockets.emit('customEvent', client.id);
+    });
 });
